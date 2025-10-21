eeeca99f2e16b85f77822f4ed85202fc3c7515bd
ezequielAtansuzsolutions <ezequiel@ansuzsolutions.com>
2025-10-21 17:46:01 -0300

    init version

---
+++ __manifest__.py
@@ -0,0 +1,45 @@
+{
+    "name": "Paraguayan Electronic Invoicing",
+    "summary": "Paraguayan Electronic Invoicing (odoo 19)",
+    "version": "1.0.0",
+    'category': 'Accounting/Localizations/EDI',
+    "author": "Ansuz Solutions",
+    "website": "https://www.ansuzsolutions.com/",
+    'description': """
+Functional
+----------
+
+Technical
+---------
+""",
+    'depends': [
+        #'l10n_ar',
+        #'certificate',
+    ],
+    'external_dependencies': {
+        #'python': ['zeep'],
+        #'apt': {
+        #    'zeep': 'python3-zeep',
+        #},
+    },
+    'data': [
+        #'wizards/l10n_ar_afip_ws_consult_view.xml',
+        #'wizards/account_move_reversal_view.xml',
+        #'views/l10n_ar_afipws_connection_view.xml',
+        #'views/res_config_settings_view.xml',
+        #'views/account_move_view.xml',
+        #'views/account_journal_view.xml',
+        #'views/res_currency_view.xml',
+        #'views/product_template_view.xml',
+        #'views/report_invoice.xml',
+        #'security/ir.model.access.csv',
+        #'data/ir_actions_act_url_data.xml',
+    ],
+    'demo': [
+        #'demo/res_company_demo.xml',
+        #'demo/res_config_settings_demo_view.xml',
+    ],
+    'installable': True,
+    #'auto_install': ['l10n_ar'],
+    "license": 'LGPL-3',
+}
